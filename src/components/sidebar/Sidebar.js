37757ad37ba40dd19d6b76d5ed0262601b53b6f1
Udhayanidhi1215 <udhayanidhi3001@gmail.com>
2022-08-22 16:58:59 +0530

    childtoparent

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,26 +1,32 @@
-import React from 'react'
-import './sidebar.css'
-import { Link } from 'react-router-dom'
-import img from '../../assets/images/boy.jpg'
+import React from "react";
+import "./sidebar.css";
+// import { Link } from "react-router-dom";
+// import img from "../../assets/images/boy.jpg";
 
 const Sidebar = () => {
-    return (
-        <div className='test'>
+  const data = "WELCOME";
+  const getData = (getData) => {
+    alert(getData);
+  };
+  return (
+    <div className="test">
+      {" "}
+      `
+      {/* <img src={img} alt="Boy" className="img"></img>
+      <ul className="side">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/login">Contact</Link>
+        </li>
+      </ul> */}
+      <button onClick={() => getData(data)}>Enter</button>
+    </div>
+  );
+};
 
-            {/* <button onClick={'/'}>Home</button>
-            <button onClick={'/about'}>About</button>
-            <button onClick={'/login'}>Contact</button> */
-    }
-            <img src={img} alt='Boy' className='img'></img>
-                    <ul className='side' > 
-                        <li><Link to='/'  >Home</Link></li>
-                        <li><Link to='/about' >About</Link></li>
-                        <li><Link to='/login' >Contact</Link></li>
-                        
-                        
-                    </ul>
-        </div>
-    )
-}
-
-export default Sidebar
+export default Sidebar;
--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,17 +1,21 @@
-import React from 'react'
-import Layout from '../../components/layout/Layout'
+import React from "react";
+// import Layout from '../../components/layout/Layout'
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Homepage = () => {
-    
-    return (
-        < Layout data="Home" color="red">
-            <div className='home'>
-                <h1 className>WELCOME</h1>
-                
-            </div>
+  const getData = (getData) => {
+    alert(getData);
+  };
+  return (
+    // < Layout data="Home" color="red">
+    //     <div className='home'>
+    //         <h1 className>WELCOME</h1>
 
-        </Layout>
-    )
-}
+    //     </div>
 
-export default Homepage
+    // </Layout>
+    <Sidebar getData={getData} />
+  );
+};
+
+export default Homepage;
